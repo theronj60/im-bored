11ff3071e1d094dc2d3a3b24dbfa258d8c67b441
Theron Joe <theronj@joetwebdev.io>
2022-05-27 10:12:11 -0600

    added response struct, created 'random' func, need to add other queries, clean up random func. WIP.

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-    "log"
 	"io/ioutil"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
+	"os"
 )
 
 // im-bored is main command
 // call https://github.com/drewthoennes/Bored-API
 // returns a suggestion
+
+type Response struct {
+	Activity      string  `json:"activity"`
+	Type          string  `json:"type"`
+	Participants  int     `json:"participants"`
+	Price         float64 `json:"price"`
+	Link          string  `json:"link"`
+	Key           string  `json:"key"`
+	Accessibility float64 `json:"accessibility"`
+}
+
+// function for calling random?
+func random() {
+	response, err := http.Get("http://www.boredapi.com/api/activity/")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject.Activity)
+	fmt.Println(responseObject.Link)
+}
 
 func main() {
 	// welcome message (ascii preferred)
@@ -21,18 +53,7 @@ func main() {
 	fmt.Println("What kind of acitvity are you looking for? (random is an option)")
 	fmt.Scanln(&activity)
 	if activity == "random" {
-		response, err := http.Get("http://www.boredapi.com/api/activity/")
-
-		if err != nil {
-			fmt.Print(err.Error())
-			os.Exit(1)
-		}
-		
-		responseData, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(responseData))
+		random()
 	}
 	// var people int
 	// fmt.Println("How many people are there? (Enter a number)")
